fdeb28d0451cb3c83bca76b35d5b110c2c11f1b8
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-10-18 14:25:09 -0300

    Implementando classes config

--- src/main/java/com/xpguardian/config/OpenAPIConfig.java
+++ src/main/java/com/xpguardian/config/OpenAPIConfig.java
@@ -1,4 +1,20 @@
 package com.xpguardian.config;
 
-public class OpenAPIConfig {
-}
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "XP Guardian API", version = "1.0", description = "API for user authentication and authorization."))
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig {}
