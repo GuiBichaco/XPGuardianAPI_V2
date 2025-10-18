1bfc67a85bac6fc29e4674f0ad074d5551985dce
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-10-18 14:22:05 -0300

    Implementando classe repository e RegisterRequest

--- src/main/java/com/xpguardian/dto/auth/RegisterRequest.java
+++ src/main/java/com/xpguardian/dto/auth/RegisterRequest.java
@@ -1,4 +1,29 @@
 package com.xpguardian.dto.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+    @NotEmpty(message = "First name cannot be empty.")
+    private String firstname;
+
+    @NotEmpty(message = "Last name cannot be empty.")
+    private String lastname;
+
+    @Email(message = "Email should be valid.")
+    @NotEmpty(message = "Email cannot be empty.")
+    private String email;
+
+    @NotEmpty(message = "Password cannot be empty.")
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
+    private String password;
 }
